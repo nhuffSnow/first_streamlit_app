6015ca057b405a7625a101673577973504e2594e
nhuffSnow <149446962+nhuffSnow@users.noreply.github.com>
2023-10-31 10:36:40 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,10 @@ streamlit.dataframe(my_fruit_list.loc[fruits_selected])
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
